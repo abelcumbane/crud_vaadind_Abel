414221deb6135a00a69145ea40f30d23c6e70cd3
Abel Cumbane <abelrosac@gmail.com>
2023-06-23 20:21:02 +0100

    chore: criacao da classe abstrata

--- src/main/java/com/example/application/data/entity/AbstractEntity.java
+++ src/main/java/com/example/application/data/entity/AbstractEntity.java
@@ -12,18 +12,18 @@ public abstract class AbstractEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgenerator")
-    // The initial value is to account for data.sql demo data ids
-    @SequenceGenerator(name = "idgenerator", initialValue = 1000)
+    // The initial value is to account for demo data ids
+    @SequenceGenerator(name = "idgenerator", initialValue = 1)
     private Long id;
 
     @Version
     private int version;
 
-    public Long getId() {
+    public  Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId( Long id) {
         this.id = id;
     }
 
